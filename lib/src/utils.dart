da4f828a751cd628a2d2cb37820a02e26cbc9686
lamnhan066 <hello@lamnhan.dev>
2025-03-07 20:03:03 +0700

    Refactor worker mapping functions for improved readability and functionality

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -6,6 +6,133 @@ void printDebug(Object? Function() log) {
   print(log());
 }
 
+/// Reads the content of a file and returns it as a list of lines
+Future<List<String>> readFileLines(String path) async {
+  final file = File(path);
+  if (!file.existsSync()) {
+    printDebug(() => 'File does not exist: $path');
+    return [];
+  }
+  return await file.readAsLines();
+}
+
+/// Writes content to a file
+Future<void> writeFile(String path, List<String> content) async {
+  final file = File(path);
+  await file.writeAsString('${content.join('\n')}\n');
+}
+
+/// Adds import statements to content if they don't already exist
+List<String> addImportStatements(
+  List<String> content,
+  String sourceFilePath,
+  String mainPath,
+) {
+  var result = List<String>.from(content);
+  var lastImportIndex = -1;
+  for (var i = 0; i < result.length; i++) {
+    if (result[i].startsWith('import ')) {
+      lastImportIndex = i;
+    }
+  }
+
+  const newImportLine =
+      "import 'package:isolate_manager/isolate_manager.dart';";
+  if (!result.contains(newImportLine)) {
+    result.insert(++lastImportIndex, newImportLine);
+  }
+
+  final newFunctionSourceImport =
+      "import '${relative(sourceFilePath, from: 'lib')}';";
+  if (absolute(sourceFilePath) != mainPath &&
+      !result.contains(newFunctionSourceImport)) {
+    result.insert(++lastImportIndex, newFunctionSourceImport);
+  }
+
+  return result;
+}
+
+/// Adds the worker mappings call to the main function
+List<String> addWorkerMappingsCall(List<String> content) {
+  var result = List<String>.from(content);
+  var mainIndex = -1;
+  for (var i = 0; i < result.length; i++) {
+    if (result[i].contains('void main(')) {
+      mainIndex = i;
+      break;
+    }
+  }
+
+  if (mainIndex == -1) {
+    printDebug(() => 'No main function found in the source file.');
+    return result;
+  }
+
+  var insertionIndex = mainIndex;
+  while (insertionIndex < result.length &&
+      !result[insertionIndex].contains(') {')) {
+    insertionIndex++;
+  }
+
+  if (insertionIndex == result.length) {
+    printDebug(() => 'Malformed main function, no opening brace found.');
+    return result;
+  }
+
+  const addWorkerMappingsCall = '  _addWorkerMappings();';
+  if (!result.any((line) => line.contains('_addWorkerMappings();'))) {
+    result.insert(insertionIndex + 1, addWorkerMappingsCall);
+  }
+
+  return result;
+}
+
+/// Adds or updates the _addWorkerMappings function
+List<String> addOrUpdateWorkerMappingsFunction(
+  List<String> content,
+  String functionName,
+) {
+  var result = List<String>.from(content);
+  final newWorkerMappingLine =
+      "  IsolateManager.addWorkerMapping($functionName, '$functionName');";
+
+  final addWorkerMappingsIndex = result.indexWhere((line) =>
+      line.replaceAll(' ', '').startsWith('void_addWorkerMappings()'));
+
+  if (addWorkerMappingsIndex == -1) {
+    // Add new function
+    result
+      ..add('')
+      ..add('/// This method MUST be stored at the end of the file to avoid')
+      ..add('/// issues when generating.')
+      ..add('void _addWorkerMappings() {')
+      ..add(newWorkerMappingLine)
+      ..add('}')
+      ..add('');
+  } else {
+    // Update existing function
+    if (!result.any((line) =>
+        line.contains(RegExp('(\'$functionName\'|"$functionName")')))) {
+      final line = result[addWorkerMappingsIndex].replaceAll(' ', '');
+      if (line.startsWith('void_addWorkerMappings(){}')) {
+        result[addWorkerMappingsIndex] = 'void _addWorkerMappings() {';
+        result.insert(addWorkerMappingsIndex + 1, newWorkerMappingLine);
+        result.insert(addWorkerMappingsIndex + 2, '}');
+      } else {
+        // Find the closing brace of the function
+        int closingBraceIndex = addWorkerMappingsIndex + 1;
+        while (closingBraceIndex < result.length &&
+            !result[closingBraceIndex].trim().startsWith('}')) {
+          closingBraceIndex++;
+        }
+        result.insert(closingBraceIndex, newWorkerMappingLine);
+      }
+    }
+  }
+
+  return result;
+}
+
 Future<void> addWorkerMappingToSourceFile(
   String workerMappingsPath,
   String sourceFilePath,
@@ -15,112 +142,15 @@ Future<void> addWorkerMappingToSourceFile(
       ? absolute(workerMappingsPath)
       : absolute('lib/main.dart');
 
-  final file = File(mainPath);
-  if (!file.existsSync()) {
-    printDebug(() => 'Source file does not exist: $mainPath');
-    return;
-  }
+  final content = await readFileLines(mainPath);
+  if (content.isEmpty) return;
 
-  // Read the existing content of the file.
-  final content = await file.readAsLines();
+  var updatedContent = addImportStatements(content, sourceFilePath, mainPath);
+  updatedContent = addWorkerMappingsCall(updatedContent);
+  updatedContent =
+      addOrUpdateWorkerMappingsFunction(updatedContent, functionName);
 
-  // Step 1: Add the import statements.
-  var lastImportIndex = -1;
-  for (var i = 0; i < content.length; i++) {
-    if (content[i].startsWith('import ')) {
-      lastImportIndex = i;
-    }
-  }
-
-  const newImportLine =
-      "import 'package:isolate_manager/isolate_manager.dart';";
-  // Insert the new import line after all existing import statements, if it doesn't exist already.
-  if (!content.contains(newImportLine)) {
-    content.insert(++lastImportIndex, newImportLine);
-  }
-
-  final newFunctionSourceImport =
-      "import '${relative(sourceFilePath, from: 'lib')}';";
-  if (absolute(sourceFilePath) != mainPath &&
-      !content.contains(newFunctionSourceImport)) {
-    content.insert(++lastImportIndex, newFunctionSourceImport);
-  }
-
-  // Step 2: Find the `main` function and add `addWorkerMappings();` at the beginning.
-  var mainIndex = -1;
-  for (var i = 0; i < content.length; i++) {
-    if (content[i].contains('void main()')) {
-      mainIndex = i;
-      break;
-    }
-  }
-
-  if (mainIndex == -1) {
-    printDebug(() => 'No main function found in the source file.');
-    return;
-  }
-
-  // Find the position after the opening brace of the `main` function.
-  var insertionIndex = mainIndex;
-  while (insertionIndex < content.length &&
-      !content[insertionIndex].contains('{')) {
-    insertionIndex++;
-  }
-
-  if (insertionIndex == content.length) {
-    printDebug(() => 'Malformed main function, no opening brace found.');
-    return;
-  }
-
-  // Add `addWorkerMappings();` if it doesn't already exist.
-  const addWorkerMappingsCall = '  _addWorkerMappings();';
-  var hasAddWorkerMappings = false;
-  for (var i = insertionIndex; i < content.length; i++) {
-    if (content[i].contains('_addWorkerMappings();')) {
-      hasAddWorkerMappings = true;
-      break;
-    }
-  }
-
-  if (!hasAddWorkerMappings) {
-    content.insert(insertionIndex + 1, addWorkerMappingsCall);
-  }
-
-  // Step 3: Collect `IsolateManager.addWorkerMapping` statements and move to `addWorkerMappings` function.
-  final newWorkerMappingLine =
-      "  IsolateManager.addWorkerMapping($functionName, '$functionName');";
-
-  // Step 4: Add or update the `addWorkerMappings` function at the end of the file.
-  final addWorkerMappingsIndex =
-      content.indexWhere((line) => line.contains('void _addWorkerMappings()'));
-
-  if (addWorkerMappingsIndex == -1) {
-    // If `addWorkerMappings` function does not exist, add it at the end.
-    content
-      ..add('')
-      ..add('/// This method MUST be stored at the end of the file to avoid')
-      ..add('/// issues when generating.')
-      ..add('void _addWorkerMappings() {')
-      ..add(newWorkerMappingLine)
-      ..add('}')
-      ..add('');
-  } else {
-    // If `addWorkerMappings` already exists, add the new worker mapping if not already present.
-    bool hasThisMapping = false;
-    for (int i = addWorkerMappingsIndex; i < content.length; i++) {
-      if (content[i].contains(RegExp('(\'$functionName\'|"$functionName")'))) {
-        hasThisMapping = true;
-        break;
-      }
-    }
-
-    if (!hasThisMapping) {
-      content.insert(addWorkerMappingsIndex + 1, newWorkerMappingLine);
-    }
-  }
-
-  // Write the modified content back to the file.
-  await file.writeAsString('${content.join('\n')}\n');
+  await writeFile(mainPath, updatedContent);
 
   printDebug(
     () =>
