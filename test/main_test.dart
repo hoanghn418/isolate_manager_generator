2ccf52c2c524683f4302b487d44ec791963d2630
lamnhan066 <hello@lamnhan.dev>
2025-03-09 08:12:06 +0700

    Add main test for listing Dart files in IsolateManagerGenerator

---
+++ test/main_test.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+import 'package:isolate_manager_generator/isolate_manager_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Main test', () {
+    test('listDartFiles', () {
+      var files = <File>[];
+      final dartFiles =
+          IsolateManagerGenerator.listDartFiles(Directory('test'), files);
+
+      expect(dartFiles.length, greaterThan(1));
+    });
+  });
+}
